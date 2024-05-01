39fa7412a23a374cb7120b6509ecd6d70842209e
Nikola Blahovicova <niki.blahovicova@gmail.com>
2024-05-01 10:56:03 +0200

    kostra syntaktickej analýzy

--- syntax_analysis/main.py
+++ syntax_analysis/main.py
@@ -1,2 +1,15 @@
 def perform_syntax_anaylsis() -> None:
-    pass
+    stack = ["PROGRAM"]
+    for token in tokens:
+        if token != stack[-1]:
+            value = rules[stack[-1] + token]
+            if value == undefined:
+                print("stack[-1] + token")
+                break
+            rule_list = value.split("=")[2].split(" ")
+            stack.pop()
+            for rule in rule_list[::-1]:
+                stack.append(rule)
+            print(value)
+        else:
+            stack.pop()
