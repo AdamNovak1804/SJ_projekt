816bb69512807466a7812d4e607346ce0dc03cf8
Nikola Blahovicova <niki.blahovicova@gmail.com>
2024-05-01 14:49:29 +0200

    odstránenie komentov a uprava main

--- syntax_analysis/main.py
+++ syntax_analysis/main.py
@@ -1,4 +1,4 @@
-def perform_syntax_anaylsis() -> None:
+def perform_syntax_anaylsis(num) -> None:
     stack = ["PROGRAM"]
     for token in tokens:
         if token != stack[-1]:
@@ -6,6 +6,9 @@ def perform_syntax_anaylsis() -> None:
             if value == undefined:
                 print("stack[-1] + token")
                 break
+            if value == "e":
+                stack.pop()
+                continue
             rule_list = value.split("=")[2].split(" ")
             stack.pop()
             for rule in rule_list[::-1]:
