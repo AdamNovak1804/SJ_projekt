7ecbffee66b616ba22fc458876817c28434aaae4
Nikola Blahovicova <niki.blahovicova@gmail.com>
2024-05-07 18:36:00 +0200

    Doplnenie korekcií

--- syntax_analysis/main.py
+++ syntax_analysis/main.py
@@ -8,6 +8,7 @@ logger = logging.getLogger('logger')
 def perform_syntax_anaylsis() -> None:
     logger.info("Performing syntax analysis....")
     tokens = read_token_stream("outputs/token_stream")
+    error_correction = 0
 
     stack = ["PROGRAM"]
     number = 0
@@ -16,7 +17,23 @@ def perform_syntax_anaylsis() -> None:
         if token != stack[-1]:
             value = rules.get(stack[-1] + token, None)
             if value == None:
-                logger.info("No rule found for", stack[-1], token)
+                if error_correction == 3:
+                    if(token != ";" and token!= "END"):
+                        logger.info(f"No rule found for {stack[-1]} {token}. Skipping token")
+                        number += 1
+                        token = tokens[number]
+                        continue
+                    else:
+                        logger.info(f"No rule found for {stack[-1]} {token}. {token} is synchronization token. Program is not valid.")
+                        break
+                elif error_correction == 4:
+                    if(tokens[number-1] not in ("number", ")", ";", "ident")):
+                        logger.info(f"No rule found for {stack[-1]} {token}. Adding semicolon")
+                        tokens.insert(number, ";")
+                        token = tokens[number]
+                        continue
+                
+                logger.info(f"No rule found for {stack[-1]} {token}")
                 break
             rule_list = value.split("=")[1].split(" ")
             logger.info(f"Value in stack {stack[-1]} exchanged with {rule_list[::-1]}") 
